4db643d3b68dde51c780054cd1799750f73046c7
Matt <97047080+mattxmade@users.noreply.github.com>
2022-04-05 18:27:23 +0100

    Create mutable array variable to hold user books

--- bookLibrary.js
+++ bookLibrary.js
@@ -1,3 +1,25 @@
+// create book constructor                        DONE
+// create array to store books
+// create function to add book to array
+// create function that loops through book array
+// display each item as a card
+// add a "NEW BOOK" button that displays a form
+  // inputs:
+    // author
+    // title
+    // number of pages
+    // read / unread
+    // year of release
+    // genre
+
+// Add remove button on book card to allow user to 
+// delete book from library
+
+// Add button to change read status
+  // function of book - read / unread
+
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
