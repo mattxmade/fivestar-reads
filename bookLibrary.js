02b47ae3a1a6b33196946d573d521d1277d27197
Matt <97047080+mattxmade@users.noreply.github.com>
2022-04-13 01:55:09 +0100

    Add data; implement card flip system; handle textfield

--- bookLibrary.js
+++ bookLibrary.js
@@ -1,32 +1,13 @@
-// create book constructor                        DONE
-// create array to store books                    DONE
-// create function to add book to array           DONE
-// create function that loops through book array  
-// display each item as a card
-// add a "NEW BOOK" button that displays a form
-  // inputs:
-    // author
-    // title
-    // number of pages
-    // read / unread
-    // year of release
-    // genre
-
-// Add remove button on book card to allow user to 
-// delete book from library
-
-// Add button to change read status
-  // function of book - read / unread
-
 let myLibrary = [];
-const buttons = [];
 
-function Book(title, author, genre, year, pages, read, rating = 0) {
+function Book(title, author, genre, year, pages, blurb, read, rating = 0) {
+
   this.title = title;
   this.author = author;
   this.genre = genre;
   this.year = year;
   this.pages = pages;
+  this.blurb = blurb;
   this.read = read;
   this.rating = rating;
 
@@ -43,11 +24,12 @@ function addBookToLibrary(...objects) {
 
 const mainContainer = document.querySelector('main');
 
-function displayBook(book, index) {
+function createCard(book, index) {
 
   const interior = document.createElement('div');
   const bookCard = document.createElement('div');
   bookCard.classList.add('books-card');
+  bookCard.id = index;
 
   const bookTitle  = document.createElement('h2');
   const bookAuthor = document.createElement('h3');
@@ -56,7 +38,25 @@ function displayBook(book, index) {
   const bookPages  = document.createElement('p');
   const bookRead   = document.createElement('p');
 
+  const bookIcon   = document.createElement('i');
+  bookIcon.classList.add('fas');
+  bookIcon.classList.add('fa-book-reader');
+  bookIcon.classList.add(`icon-set_${index}`);
+
   const bookRemove = document.createElement('button');
+
+  // card__face card__face--front
+  // Front
+  const cardFront = document.createElement('div');
+  cardFront.classList.add('card__book--front');
+
+  const cardBack  = document.createElement('div');
+  cardBack.classList.add('card__book--back');
+
+  const bookBlurb  = document.createElement('p');
+  bookBlurb.textContent = book.blurb;
+  cardBack.appendChild(bookBlurb);
+  //
 
   bookTitle.textContent = book.title;
   bookAuthor.textContent = book.author;
@@ -67,17 +67,19 @@ function displayBook(book, index) {
   bookGenre.textContent = book.genre;
   
   bookYear.textContent = book.year;
-  bookPages.textContent = book.pages;
+  bookPages.textContent = `${book.pages} pages`;
   bookRead.textContent = 'read';
   bookRemove.textContent = 'remove';
 
-  bookCard.appendChild(interior);
+  const background = setBackground(book.genre);
 
-  interior.style.background = `linear-gradient(45deg, rgb(255, 255, 255) 30%, ${setBackground(book.genre)} 30%, rgb(110, 140, 180))`;
+  interior.style.background = `linear-gradient(45deg, rgb(255, 255, 255) 30%, ${background} 30%, rgb(75, 75, 75))`;
+  cardBack.style.background = `linear-gradient(45deg, ${background} 30%, rgb(75, 75, 75))`;
+  cardFront.appendChild(interior);
 
-  bookCard.appendChild(bookTitle);
-  bookCard.appendChild(bookAuthor);
-  bookCard.appendChild(bookGenre);
+  cardFront.appendChild(bookTitle);
+  cardFront.appendChild(bookAuthor);
+  cardFront.appendChild(bookGenre);
 
   const ratingHolder = document.createElement('div');
   ratingHolder.classList.add('star-rating');
@@ -91,10 +93,10 @@ function displayBook(book, index) {
     ratingHolder.appendChild(starRating);
   }
 
-  bookCard.appendChild(ratingHolder);
+  cardFront.appendChild(ratingHolder);
 
-  bookCard.appendChild(bookYear);
-  bookCard.appendChild(bookPages);
+  cardFront.appendChild(bookYear);
+  cardFront.appendChild(bookPages);
 
   // read status
   const statusIcon = document.createElement('i');
@@ -113,9 +115,20 @@ function displayBook(book, index) {
   readStatus.appendChild(bookRead);
   readStatus.appendChild(statusIcon);
 
-  bookCard.appendChild(readStatus);
-  bookCard.appendChild(bookRemove);
+  cardFront.appendChild(readStatus);
+  cardFront.appendChild(bookRemove);
+
+  cardFront.appendChild(bookIcon);
+  cardBack.appendChild(bookIcon.cloneNode(true));
   
+  // Front + Back
+  // Front
+  bookCard.appendChild(cardFront);
+
+  // Back
+  bookCard.appendChild(cardBack);
+  //
+
   const firstNode = document.querySelector('.placeholder');
   mainContainer.insertBefore(bookCard, firstNode);
 
@@ -134,13 +147,16 @@ function displayBook(book, index) {
     else {
       book.read = 'unread';
     }
+    console.log(`${book.title} status: ${book.read}`);
   });
 
   // remove button listener
-  bookRemove.addEventListener('click', () => {
+  bookRemove.addEventListener('click', (e) => {
+
     mainContainer.removeChild(bookCard);
-    
-    myLibrary.splice(index, index+1);
+
+    delete myLibrary[index];
+    console.table(myLibrary);
   });
 
   // rating nodelist
@@ -148,41 +164,27 @@ function displayBook(book, index) {
 
   setRating(stars, book.rating);
   updateRating(book, stars, book.rating);
+
+  const icons = document.querySelectorAll(`.icon-set_${index}`);
+  
+  icons.forEach(icon => {
+    icon.addEventListener('click', () => {
+      bookCard.classList.toggle('is-flipped');
+
+      cardFront.childNodes.forEach(child => {
+        // console.dir(child);
+
+        if (bookCard.classList.contains('is-flipped')) {
+          child.classList.add('hidden');
+        }
+        else { child.classList.remove('hidden'); }
+        
+      });
+    });  
+  })
+
 }
 
-function setBackground(string) {
-  switch(string) {
-    case 'Horror':
-      return 'rgb(130, 40, 40)';
-
-    case 'Comedy':
-      return 'rgb(130, 40, 111)';
-       
-    case 'Fantasy':
-      return 'rgb(40, 80, 130)'; 
-      
-    case 'Romance':
-      return 'rgb(40, 42, 130)';
-      
-    case 'Historical':
-      return 'rgb(130, 73, 40)';
-
-    case 'Biography':
-      return 'rgb(72, 40, 130)'; 
-      
-    case 'Sci-Fi':
-      return 'rgb(40, 130, 123)';
-
-    case 'Crime-Mystery':
-      return 'rgb(40, 109, 130)';
-    
-    case 'Action-Adventure':
-      return 'rgb(163, 89, 89)';
-    
-    default:
-      return 'rgb(40, 80, 130)';
-  }
-}
 
 function setRating(array, number) {
 
@@ -220,7 +222,7 @@ function updateRating(book, stars, lastRating) {
       }
 
       lastRating = book.rating;
-      // console.log(book.rating);
+      console.log(`${book.title} rating: ${book.rating}`);
     });
   });
 }
@@ -239,8 +241,8 @@ function setFontStyle(property) {
     case 'Romance':
       return 'book-romance';
       
-    case 'Historical':
-      return 'book-historical';
+    case 'Educational':
+      return 'book-educational';
 
     case 'Biography':
       return 'book-biography'; 
@@ -256,6 +258,41 @@ function setFontStyle(property) {
   }
 }
 
+function setBackground(string) {
+  switch(string) {
+    case 'Horror':
+      return 'rgb(130, 40, 40)';
+
+    case 'Comedy':
+      return 'rgb(130, 40, 111)';
+       
+    case 'Fantasy':
+      return 'rgb(40, 80, 130)'; 
+      
+    case 'Romance':
+      return 'rgb(40, 42, 130)';
+      
+    case 'Educational':
+      return 'rgb(130, 73, 40)';
+
+    case 'Biography':
+      return 'rgb(72, 40, 130)'; 
+      
+    case 'Sci-Fi':
+      return 'rgb(40, 130, 123)';
+
+    case 'Crime-Mystery':
+      return 'rgb(40, 109, 130)';
+    
+    case 'Action-Adventure':
+      return 'rgb(163, 89, 89)';
+    
+    default:
+      return 'rgb(40, 80, 130)';
+  }
+}
+
+// Modal
 const modal = document.querySelector('#js-modal');
 const formButtons = document.querySelectorAll('.form-button');
 const backButton = document.querySelector('#js-back-button');
@@ -273,62 +310,117 @@ function hideForm(e) {
   modal.close();
 }
 
+// Form
 const form = document.querySelector('form');
 const inputs = form.elements;
 
 form.addEventListener(('submit'), (e) => {
   e.preventDefault();
 
-  let selectGenre;
+  // check if title exists - we don't want duplicates
+  let exists = false;
 
-  const options = document.querySelector('select');
-  for ( option of options.selectedOptions ) {
-    selectGenre = option.attributes.value.nodeValue
-    // console.log(genre);
+  myLibrary.forEach(item => {
+    if (String(item.title).toLowerCase() === String(inputs["title"].value).toLowerCase()) {
+      exists = true;
+    }
+  })
+
+  if (!exists) {
+    let selectGenre;
+
+    const options = document.querySelector('select');
+    for ( option of options.selectedOptions ) {
+      selectGenre = option.attributes.value.nodeValue
+    }
+
+    const title  = inputs["title"].value;
+    const author = inputs["author"].value;
+    const year   = Number(inputs["year"].value);
+    const genre  = selectGenre;
+    const pages  = Number(inputs["pages"].value);
+    const blurb  = inputs["blurb"].value;
+    const status = inputs["status"].value;
+
+    // console.table({
+    //   title, author, year, genre, pages, blurb, status
+    // });
+
+    const userBook = new Book(title, author, genre, year, pages, blurb, status);
+    // console.log(userBook.info());
+    
+    addBookToLibrary(userBook);
+    console.table(myLibrary);
+
+    createCard(myLibrary[myLibrary.length - 1], myLibrary.length - 1);
   }
 
-  const title  = inputs["title"].value;
-  const author = inputs["author"].value;
-  const year   = inputs["year"].value;
-  const genre  = selectGenre;
-  const pages  = inputs["pages"].value;
-  const blurb  = inputs["blurb"].value;
-  const status = inputs["status"].value;
-
-  console.table({
-    title, author, year, genre, pages, blurb, status
-  });
-
-  const userBook = new Book(title, author, genre, year, pages, status);
-  console.log(userBook.info());
-  
-  addBookToLibrary(userBook);
-  console.table(myLibrary);
-
-  displayBook(myLibrary[myLibrary.length - 1], myLibrary.length - 1);
-
 });
-
-const AGOT = new Book(
-  'A Game Of Thrones', 'George R.R. Martin', "Fantasy", 1996, 694, "read", 4);
-
-addBookToLibrary(AGOT);
-displayBook(myLibrary[myLibrary.length - 1], myLibrary.length - 1);
 
 const removeAll = document.getElementById('js-remove-all');
 
 removeAll.addEventListener('click', () => {
-  const cards = document.querySelectorAll('.books-card');  
+  const cards = document.querySelectorAll('.books-card'); 
 
   if (myLibrary.length > 0) {
 
-    console.log(cards)
-    cards.forEach((card, index) => {
-
-      myLibrary.splice(index, index+1);
-      mainContainer.removeChild(card);
-
+    myLibrary.forEach(book => {
+      console.log(`${book.title} has been removed`);
     });
 
+    cards.forEach((card, index) => {
+
+      mainContainer.removeChild(card);
+      myLibrary = [];
+    });
   }
+});
+
+// Data
+const AGOT = new Book(
+  'A Game of Thrones', 'George R.R. Martin', "Fantasy", 1996, 694, 
+  "Amid plots and counterplots, tragedy and betrayal, victory and terror, the fate of the Starks, their allies, and their enemies hangs perilously in the balance, as each endeavors to win that deadliest of conflicts: the game of thrones.",
+  "read", 5);
+
+const DRAC = new Book(
+  'Dracula', 'Bram Stoker', "Horror", 1897, 418, 
+  "Dracula is a classic and genre-defining tale of horror from famed author Bram Stoker. This novel contains the chilling tales of those that encountered the monster Dracula on his quest to emigrate from Transylvania to England, as he seeks to consume blood and spread his undead curse to the innocent.", 
+  "read", 4);
+
+const DUNE = new Book(
+  'Dune', 'Frank Herbert', "Sci-Fi", 1965, 412, 
+  "Set on the desert planet Arrakis, Dune is the story of the boy Paul Atreides, heir to a noble family tasked with ruling an inhospitable world where the only thing of value is the “spice” melange, a drug capable of extending life and enhancing consciousness.", 
+  "read", 4);
+
+
+const ADWD = new Book(
+  'A Dance with Dragons', 'George R.R. Martin', "Fantasy", 2011, 761,
+  "From all corners, bitter conflicts reignite, intimate betrayals are perpetrated, and a grand cast of outlaws and priests, soldiers and skinchangers, nobles and slaves, will face seemingly insurmountable obstacles. Some will fail, others will grow in the strength of darkness.",
+  "unread", 0);
+  
+const OLDR = new Book(
+  'Outlander', 'Diana Gabaldon', "Fantasy", 1991, 850,
+  "The year is 1945. Claire Randall, a former combat nurse, is just back from the war and reunited with her husband on a second honeymoon when she walks through a standing stone in one of the ancient circles that dot the British Isles. Suddenly she is a Sassenach—an “outlander”.",
+  "read", 3);
+
+const PRAP = new Book(
+  'Pride & Prejudice', 'Jane Austen', "Romance", 1813, 761,
+  "Pride and Prejudice follows the turbulent relationship between Elizabeth Bennet, the daughter of a country gentleman, and Fitzwilliam Darcy, a rich aristocratic landowner. They must overcome the titular sins of pride and prejudice in order to fall in love and marry.",
+  "unread", 0);
+
+const DKJS = new Book(
+  'Have a Nice Day: A Tale of Blood and Sweatsocks', 'Mick Foley', "Biography", 1999, 554,
+  "Mick Foley is a nice man, a family man who loves amusement parks and eating ice cream in bed. Here is an intimate glimpse into Mick Foley's mind, his history, his work and what some might call his pathology.",
+  "read", 4);
+
+const MDLA = new Book(
+  'Nelson Mandela', 'Martin Meredith', "Educational", 1997, 653,
+  "The riveting memoirs of the outstanding moral and political leader of our time, A Long Walk to Freedom brilliantly re-creates the drama of the experiences that helped shape Nelson Mandela's destiny.",
+  "read", 5);
+
+
+addBookToLibrary(AGOT, DRAC, DUNE, ADWD, OLDR, PRAP, DKJS, MDLA);
+
+myLibrary.forEach((book, index) => {
+  createCard(book, index);
 });
